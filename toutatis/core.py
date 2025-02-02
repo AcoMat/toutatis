2bc0992cd18c7a6f32d3c22fbfd080918d9e57cd
Acosta Matias <aaco.maty@gmail.com>
2025-02-02 16:02:04 -0300

    Update core.py

--- toutatis/core.py
+++ toutatis/core.py
@@ -24,7 +24,7 @@ print("Follower               : " + str(infos["follower_count"]) + " | Following
 print("Number of posts        : " + str(infos["media_count"]))
 
 # Verificar si la URL externa existe
-if infos["external_url"]:
+if infos.get("external_url"):
     print("External url           : " + infos["external_url"])
 
 print("IGTV posts             : " + str(infos["total_igtv_videos"]))
@@ -34,10 +34,10 @@ print("Memorial Account       : " + str(infos["is_memorialized"]))
 print("New Instagram user     : " + str(infos["is_new_to_instagram"]))
 
 # Verificar si 'public_email' y 'public_phone_number' existen
-if "public_email" in infos.keys() and infos["public_email"]:
+if "public_email" in infos and infos["public_email"]:
     print("Public Email           : " + infos["public_email"])
 
-if "public_phone_number" in infos.keys() and str(infos["public_phone_number"]):
+if "public_phone_number" in infos and str(infos["public_phone_number"]):
     phonenr = "+" + str(infos["public_phone_country_code"]) + " " + str(infos["public_phone_number"])
     try:
         pn = phonenumbers.parse(phonenr)
@@ -50,21 +50,21 @@ if "public_phone_number" in infos.keys() and str(infos["public_phone_number"]):
 
 other_infos = advanced_lookup(infos["username"])
 
-if other_infos["error"] == "rate limit":
+if other_infos.get("error") == "rate limit":
     print("Rate limit please wait a few minutes before you try again")
-elif "message" in other_infos["user"].keys():
-    if other_infos["user"]["message"] == "No users found":
+elif "message" in other_infos.get("user", {}):
+    if other_infos["user"].get("message") == "No users found":
         print("The lookup did not work on this account")
     else:
         print(other_infos["user"]["message"])
 else:
-    if "obfuscated_email" in other_infos["user"].keys():
+    if "obfuscated_email" in other_infos.get("user", {}):
         if other_infos["user"]["obfuscated_email"]:
             print("Obfuscated email       : " + other_infos["user"]["obfuscated_email"])
         else:
             print("No obfuscated email found")
 
-    if "obfuscated_phone" in other_infos["user"].keys():
+    if "obfuscated_phone" in other_infos.get("user", {}):
         if str(other_infos["user"]["obfuscated_phone"]):
             print("Obfuscated phone       : " + str(other_infos["user"]["obfuscated_phone"]))
         else:
